2f3e3e59cb177345cffc5ff0f04d4e0f2e866b30
lorenzosinisi <lasslo.net@gmail.com>
2021-01-13 16:10:12 +0100

    Configure hex package

--- mix.exs
+++ mix.exs
@@ -6,7 +6,34 @@ defmodule Woke.MixProject do
       app: :woke,
       version: "0.1.0",
       elixir: "~> 1.10",
+      deps: deps(),
+      ddocs: [extras: ["README.md"], main: "readme", source_ref: "v0.1.0"],
+      source_url: "https://github.com/lorenzosinisi/woke",
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp description do
+    """
+    Watchdog design patter library for Elixir applications
+    """
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/woke"}
     ]
   end
 
